9ff793c112df8ab6a49124cf01d7845583acbd62
ygsanabria8 <yesid251@hotmail.com>
2024-08-08 23:04:39 -0500

    feat(infrastructure_module.go): provide mongo finder and repository to fx context

--- src/fxmodule/infrastructure_module.go
+++ src/fxmodule/infrastructure_module.go
@@ -1,7 +1,9 @@
 package fxmodule
 
 import (
+	"api.ddd/src/infrastructure/finder/mongo"
 	"api.ddd/src/infrastructure/register_services"
+	repository "api.ddd/src/infrastructure/repository/mongo"
 	"go.uber.org/fx"
 )
 
@@ -9,4 +11,6 @@ var InfrastructureModule = fx.Module(
 	"Infrastructure Module",
 	fx.Provide(register_services.ProvideMongoClient),
 	fx.Provide(register_services.ProvideSqlClient),
+	fx.Provide(finder.NewMongoFinder),
+	fx.Provide(repository.NewMongoRepository),
 )
