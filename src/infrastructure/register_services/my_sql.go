6a7483cf1203fd5d350306f73507a5ec593e7f32
ygsanabria8 <yesid251@hotmail.com>
2024-08-12 14:31:37 -0500

    feat(my_sql.go): add database name and auto migrate user structure

--- src/infrastructure/register_services/my_sql.go
+++ src/infrastructure/register_services/my_sql.go
@@ -2,6 +2,7 @@ package register_services
 
 import (
 	"api.ddd/src/api/server"
+	"api.ddd/src/domain/aggregates"
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -10,11 +11,12 @@ import (
 // ProvideSqlClient Provide Sql Client Instance
 func ProvideSqlClient(logger *server.Logger) *gorm.DB {
 	dsn := fmt.Sprintf(
-		"%s:%s@tcp(%s:%d)?charset=utf8&parseTime=True&loc=Local",
+		"%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local",
 		"admin",
 		"123456",
 		"127.0.0.1",
 		3306,
+		"user",
 	)
 	client, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -25,6 +27,12 @@ func ProvideSqlClient(logger *server.Logger) *gorm.DB {
 	db, _ := client.DB()
 	db.SetMaxIdleConns(50)
 	db.SetMaxOpenConns(50)
+
+	err = client.AutoMigrate(&aggregates.User{})
+	if err != nil {
+		logger.Fatalf("Cannot Migrate Into Sql %v", err.Error())
+		panic(err.Error())
+	}
 
 	return client
 }
