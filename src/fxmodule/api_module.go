8c6f3f5baa36e887316f487e88cdef6ad604bd05
ygsanabria8 <yesid251@hotmail.com>
2024-08-12 15:03:41 -0500

    refactor(application_module.go): move handler registrations into application module

--- src/fxmodule/api_module.go
+++ src/fxmodule/api_module.go
@@ -5,8 +5,6 @@ import (
 	"api.ddd/src/api/controllers"
 	"api.ddd/src/api/routes"
 	"api.ddd/src/api/server"
-	command "api.ddd/src/application/command/user"
-	query "api.ddd/src/application/query/user"
 	"go.uber.org/fx"
 )
 
@@ -18,20 +16,4 @@ var ApiModule = fx.Module(
 	fx.Provide(controllers.NewController),
 	fx.Invoke(server.ConfigureCors),
 	fx.Invoke(routes.ConfigureRoutes),
-	fx.Invoke(RegisterHandlers),
 )
-
-func RegisterHandlers(
-	dispatcher mediator.IDispatcher,
-	createUserCommandHandler *command.CreateUserCommandHandler,
-	updateUserCommandHandler *command.UpdateUserCommandHandler,
-	deleteUserCommandHandler *command.DeleteUserCommandHandler,
-	getUserByIdQueryHandler *query.GetUserByIdQueryHandler,
-	getAllUsersQueryHandler *query.GetAllUsersQueryHandler,
-) {
-	_ = dispatcher.RegisterHandler(createUserCommandHandler, &command.CreateUserCommand{})
-	_ = dispatcher.RegisterHandler(updateUserCommandHandler, &command.UpdateUserCommand{})
-	_ = dispatcher.RegisterHandler(deleteUserCommandHandler, &command.DeleteUserCommand{})
-	_ = dispatcher.RegisterHandler(getUserByIdQueryHandler, &query.GetUserByIdQuery{})
-	_ = dispatcher.RegisterHandler(getAllUsersQueryHandler, &query.GetAllUsersQuery{})
-}
--- src/fxmodule/application_module.go
+++ src/fxmodule/application_module.go
@@ -1,6 +1,7 @@
 package fxmodule
 
 import (
+	"api.ddd/pkgs/mediator"
 	command "api.ddd/src/application/command/user"
 	query "api.ddd/src/application/query/user"
 	"go.uber.org/fx"
@@ -17,4 +18,20 @@ var ApplicationModule = fx.Module(
 		query.NewGetUserByIdQueryHandler,
 		query.NewGetAllUsersQueryHandler,
 	),
+	fx.Invoke(RegisterHandlers),
 )
+
+func RegisterHandlers(
+	dispatcher mediator.IDispatcher,
+	createUserCommandHandler *command.CreateUserCommandHandler,
+	updateUserCommandHandler *command.UpdateUserCommandHandler,
+	deleteUserCommandHandler *command.DeleteUserCommandHandler,
+	getUserByIdQueryHandler *query.GetUserByIdQueryHandler,
+	getAllUsersQueryHandler *query.GetAllUsersQueryHandler,
+) {
+	_ = dispatcher.RegisterHandler(createUserCommandHandler, &command.CreateUserCommand{})
+	_ = dispatcher.RegisterHandler(updateUserCommandHandler, &command.UpdateUserCommand{})
+	_ = dispatcher.RegisterHandler(deleteUserCommandHandler, &command.DeleteUserCommand{})
+	_ = dispatcher.RegisterHandler(getUserByIdQueryHandler, &query.GetUserByIdQuery{})
+	_ = dispatcher.RegisterHandler(getAllUsersQueryHandler, &query.GetAllUsersQuery{})
+}
