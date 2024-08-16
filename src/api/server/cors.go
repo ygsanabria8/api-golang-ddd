66190ab69cc4a029afa644e35c0b5575f1f6c416
ygsanabria8 <yesid251@hotmail.com>
2024-08-16 17:34:19 -0500

    fix(cors.go): change way to set origins and headers

--- src/api/server/cors.go
+++ src/api/server/cors.go
@@ -11,9 +11,9 @@ func ConfigureCors(server *gin.Engine, config *Configuration) {
 	server.Use(
 		cors.New(
 			cors.Config{
-				AllowOrigins:     []string{config.Server.Origin},
+				AllowOrigins:     config.Server.Origin,
 				AllowMethods:     []string{http.MethodGet, http.MethodPatch, http.MethodPost, http.MethodDelete},
-				AllowHeaders:     []string{config.Server.Header},
+				AllowHeaders:     config.Server.Header,
 				AllowCredentials: false,
 			},
 		),
