00d2df1fc6849241476c158484c839754f38106d
ygsanabria8 <yesid251@hotmail.com>
2024-08-21 22:04:53 -0500

    refactor(message bus): change implementation from event to Ievent interface

--- src/api/consumers/user.go
+++ src/api/consumers/user.go
@@ -5,7 +5,7 @@ import (
 	"api.ddd/src/domain/events"
 )
 
-func (e *CreatedUserEventMessage) OnMessage(event *utils.Event) {
+func (e *CreatedUserEventMessage) OnMessage(event utils.IEvent) {
 	e.logger.Infof("Invoked CreatedUserEventMessage Consumer")
 
 	var message *events.CreatedUserEvent
@@ -18,7 +18,7 @@ func (e *CreatedUserEventMessage) OnMessage(event *utils.Event) {
 	e.logger.Infow("CreatedUserEventMessage", message.User)
 }
 
-func (e *UpdatedUserEventMessage) OnMessage(event *utils.Event) {
+func (e *UpdatedUserEventMessage) OnMessage(event utils.IEvent) {
 	e.logger.Infof("Invoked CreatedUserEventMessage Consumer")
 
 	var message *events.UpdatedUserEvent
@@ -31,7 +31,7 @@ func (e *UpdatedUserEventMessage) OnMessage(event *utils.Event) {
 	e.logger.Infow("UpdatedUserEventMessage", message.User)
 }
 
-func (e *DeletedUserEventMessage) OnMessage(event *utils.Event) {
+func (e *DeletedUserEventMessage) OnMessage(event utils.IEvent) {
 	e.logger.Infof("Invoked CreatedUserEventMessage Consumer")
 
 	var message *events.DeletedUserEvent
